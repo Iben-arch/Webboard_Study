80b467e0a774d9bf04c1e6f36279a66580d3fcb7
Iben-arch <bourbok49@gmail.com>
2024-08-13 11:02:36 +0700

    edit
    edit code

--- delete.php
+++ delete.php
@@ -1,10 +1,9 @@
 <?php
     session_start();
-    if(!$_SESSION['role'] == "a"){
+    if(isset($_SESSION['id']) && $_SESSION['role'] == "a"){
+        echo "ลบกระทู้ หมายเลข&nbsp$_GET[id]";
+        echo "<br><a href='index.php'>กลับไปหน้าหลัก</a>";
+    }else{
         header("location:index.php");
-    }
-?>
-<?php
-    echo "ลบกระทู้ หมายเลข&nbsp$_GET[id]";
-    echo "<br><a href='index.php'>กลับไปหน้าหลัก</a>";
+}
 ?>
