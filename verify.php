3ceb3db9d0757e62a4ffbac434528f787220a67a
Iben-arch <bourbok49@gmail.com>
2024-10-01 11:02:55 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -16,32 +16,28 @@
     <?php
     $L = $_POST['Login'];
     $P = $_POST['Password'];
-    $login = $_POST['login'];
-    $pwd = $_POST['pwd'];
+    // $login = $_POST['login'];
+    // $pwd = $_POST['pwd'];
 
-    $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
-    $sql = "SELECT * FROM user where login='$login' and password='$pwd'";
-    $result = $conn->query($sql);
-    if($result->rowCount()==1){
-        $data=$result->fetch(PDO::FETCH_ASSOC);
-        $_SESSION['username']=$data['login'];
-        $_SESSION['role']=$data['role'];
-        $_SESSION['user_id']=$data['id'];
-        $_SESSION['id']=$_SESSION_id();
-        header("location:index.php");
-        die();
-    }else{
-        $_SESSION['Error']="error";
-        header("location:login.php");
-        die();
-    }
-    // echo"Webboard Suisei"."<HR>";
-    // echo"เข้าสู่ระบบด้วย"."<BR>";
-    // echo"Login = $L"."<BR>";
-    // echo"Password = $P"."<BR>";
-    $conn = null;
+    // $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+    // $sql = "SELECT * FROM user where login='$login' and password='$pwd'";
+    // $result = $conn->query($sql);
+    // if($result->rowCount()==1){
+    //     $data=$result->fetch(PDO::FETCH_ASSOC);
+    //     $_SESSION['username']=$data['login'];
+    //     $_SESSION['role']=$data['role'];
+    //     $_SESSION['user_id']=$data['id'];
+    //     $_SESSION['id']=$_SESSION_id();
+    //     header("location:index.php");
+    //     die();
+    // }else{
+    //     $_SESSION['Error']="error";
+    //     header("location:login.php");
+    //     die();
+    // }
+    // $conn = null;
     ?>
-        <!-- <?php
+        <?php
         if($L == "admin" && $P == "ad1234"){
             $_SESSION['username'] = "admin";
             $_SESSION['role'] = 'a';
@@ -58,6 +54,6 @@
             $_SESSION['Error'] = 1;
             header("location:login.php");
         }
-        ?> -->
+        ?>
 </body>
 </html>
