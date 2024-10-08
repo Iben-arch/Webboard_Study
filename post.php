b45a5eba29942f085cc6b831a40cc3ebbb1b8a9c
Iben-arch <bourbok49@gmail.com>
2024-10-08 10:11:34 +0700

    Update post.php

--- post.php
+++ post.php
@@ -1,29 +1,32 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Post</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1 style="text-align: center;">Webboard Suisei</h1><hr>
-    <div style="text-align: center;">
-        ต้องการดูกระทู้หมายเลข <?php echo $_GET['id']?><br>
+    <?php
+    include "nav.php";
+    ?>
+    <div class="card border-primary mt-4 m-auto" style="max-width: 30rem;">
+        <div class=" card-header border-primary bg-primary text-white">แสดงความคิดเห็น</div>
+        <form action="index.php" method="post">
+            <div class=" card-body">
+                <textarea name="message" class=" form-control" rows="5" required></textarea>
+            </div>
+            <div class=" m-lg-2">
+                <center>
+                    <button type="submit" class=" btn btn-primary btn-sm text-white"><i class="bi bi-box-arrow-in-left"></i>ส่งข้อความ</button>
+                </center>
+            </div>            
+        </form>
     </div>
-    <table style="border: 2px solid black; width: 40%;" align="center">
-        <tr><th style="background-color: #6CD2FE; text-align:left" colspan="2">แสดงความคิดเห็น</th></tr><br>
-        <div style="text-align: center;">
-        <?php
-        if(($_GET['id'] % 2) == 0)
-            echo "เป็นกระทู้หมายเลขคู่"."<BR>";
-        else
-            echo "เป็นกระทู้หมายเลขคี่"."<BR>";
-        ?></div>
-        <tr><td><form>
-            <textarea name="message" rows="10" cols="100"></textarea>
-        </form></td></tr><br>
-        <tr><td colspan="2" align="center"><input type="submit" value="ส่งข้อความ"></td></tr></table>
-    <br>
-    <div style="text-align: center;"><a href="index.php" >กลับไปหน้าหลัก</a></div>
 </body>
 </html>
