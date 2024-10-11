460caef0e8547cc3a57afcb4aafe980fecaddbd4
Iben-arch <bourbok49@gmail.com>
2024-10-11 16:44:11 +0700

    ีupdate

--- delete.php
+++ delete.php
@@ -1,9 +1,16 @@
 <?php
     session_start();
-    if(isset($_SESSION['id']) && $_SESSION['role'] == "a"){
-        echo "ลบกระทู้ หมายเลข&nbsp$_GET[id]";
-        echo "<br><a href='index.php'>กลับไปหน้าหลัก</a>";
+    if(isset($_SESSION['role']) && $_SESSION['role'] == 'a'){
+        $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+        $sql = "DELETE FROM post where id = $_GET[id]";
+        $conn->exec($sql);
+        $sql1 = "DELETE FROM comment where post_id = $_GET[id]";
+        $conn->exec($sql1);
+        $conn=null;
+        header("location:index.php");
     }else{
         header("location:index.php");
-}
+        die();
+    }
+    
 ?>
--- post.php
+++ post.php
@@ -12,31 +12,67 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 <body>
-    <div class=" card text-dark bg-white border-success">
-        <div class=" card-header bg-success text-white">แสดงความคิดเห็น</div>
-        <div class=" card-body">
-            <form action="post_save.php" method="post">
-            <input type="hidden" name="post_id" value="<?= $_GET['id']; ?>">
-                <div class=" row mb-3 justify-content-center">
-                    <div class=" col-lg-10">
-                        <textarea name="comment" class=" form-control" rows="8"></textarea>
-                    </div>
+    <div class=" container-lg">
+        <?php
+            include "nav.php";
+        ?>
+        <div class=" col-lg-6 col-md-8 col-sm-10 mx-auto">
+            <div class=" card text-dark bg-white border-info m-auto mt-3">
+                <?php
+                    $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+                    $sql = "SELECT t2.login,t1.post_date,t1.title,t1.content FROM post as t1 INNER JOIN user as t2 ON (t1.user_id=t2.id)
+                            where t1.id=$_GET[id]" ;
+                    $result = $conn->query($sql);
+                    while($row = $result->fetch()){
+                        echo "<div class='card-header bg-info text-black'><h5>$row[2]</h5></div>
+                            <div class='card-body'>$row[3]</div>
+                        <div class='card-footer'>[$row[0]] - $row[1]</div>";
+                    }
+
+                $conn=null;
+                ?>
+            </div>
+            <?php
+                $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+                $sql1 = "SELECT comment.id,comment.content,user.login,comment.post_date FROM comment INNER JOIN user ON (comment.user_id=user.id)
+                    INNER JOIN post ON (comment.post_id=post.id) where comment.post_id=$_GET[id] ORDER BY comment.post_date ASC";
+                $result = $conn->query($sql1);
+                $i=1;
+                    while($row = $result->fetch()){
+                        echo "<div class='card text-dark bg-white border-primary mt-3'>
+                                <div class='card-header bg-primary text-white'>ความคิดเห็นที่ $i</div>
+                                <div class='card-body'>$row[1]</div>
+                                <div class='card-footer'>[$row[2]]-$row[3]</div>
+                                </div>";
+                        $i++;
+                        }
+                        $conn=null;
+                    ?>
+            <div class=" card text-dark bg-white border-success m-auto mt-3" style="max-width: 50rem;">
+                <div class=" card-header bg-success text-white">แสดงความคิดเห็น</div>
+                <div class=" card-body">
+                    <form action="post_save.php" method="post">
+                    <input type="hidden" name="post_id" value="<?= $_GET['id']; ?>">
+                        <div class=" row mb-3 justify-content-center">
+                            <div class=" col-lg-10">
+                                <textarea name="comment" class=" form-control" rows="8"></textarea>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class=" col-lg-12">
+                                <center>
+                                    <button type="submit" class=" btn btn-success btn-sm text-white">
+                                        <i class="bi bi-box-arrow-up-right me-1"></i>ส่งข้อความ
+                                    </button>
+                                </center>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                <div class="row">
-                    <div class=" col-lg-12">
-                        <center>
-                            <button type="submit" class=" btn btn-success btn-sm text-white">
-                                <i class="bi bi-box-arrow-up-right me-1"></i>ส่งข้อความ
-                            </button>
-                        </center>
-                    </div>
-                </div>
-            </form>
+            </div>
         </div>
     </div>
-    <!-- <?php
-    include "nav.php";
-    ?>
+    <!--
     <div class="card border-primary mt-4 m-auto" style="max-width: 30rem;">
         <div class=" card-header border-primary bg-primary text-white">แสดงความคิดเห็น</div>
         <form action="index.php" method="post">
